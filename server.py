5e59303ed219204c780b348f0c28944aff9438b2
Denisenko N <info.denisenko@gmail.com>
2020-11-11 00:17:16 +0300

    'fixed'

--- server.py
+++ server.py
@@ -27,5 +27,12 @@ def server_fail():
 	return "fail! fail! fail!"
 
 
-if __name__ == "__main__":
-	app.run(host='localhost', port=8080, debug=True)
+if os.environ.get("APP_LOCATION") == "heroku":
+    run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        server="gunicorn",
+        workers=3,
+    )
+else:
+    run(host="localhost", port=8080, debug=True)
