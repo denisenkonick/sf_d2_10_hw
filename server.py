e0f45c921787ff1449f6170613c47afe55ebdb41
Denisenko N <info.denisenko@gmail.com>
2020-11-14 18:32:34 +0300

    added logging with Sentry

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from bottle import Bottle, route, HTTPResponse, static_file
 import os
+import sentry_sdk
 
 app = Bottle()
 
@@ -11,15 +12,15 @@ def index():
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>D2 Temnov</title>
+        <title>D2 Denisenko</title>
         <link rel="stylesheet" href="/static/style.css" type="text/css">
     </head>
     <body>
         <h1>This is main page</h1>
         <hr>
-        <h2>For visit successful page, append to the end of URL "https://young-journey-39325.herokuapp.com/<span style="color: lawngreen;">/success</span>"</h2>
+        <h2>For visit successful page, append to the end of URL "https://young-journey-39325.herokuapp.com<span style="color: lawngreen;">/success</span>"</h2>
         <hr>
-        <h2>For visit fail page, append to the end of URL "https://young-journey-39325.herokuapp.com/<span>/fail</span>"</h2>
+        <h2>For visit fail page, append to the end of URL "https://young-journey-39325.herokuapp.com<span>/fail</span>"</h2>
         <hr>
     </body>
 </html>
@@ -41,6 +42,9 @@ def server_fail():
     return HTTPResponse(status=500, body="Fail page")
 
 if os.environ.get("APP_LOCATION") == "heroku":
+    sentry_sdk.init(dsn=os.environ['SENTRY_DSN'],
+                    integrations=[BottleIntegration()]
+                    )
     app.run(
         host='0.0.0.0',
         port=int(os.environ.get('PORT', 5000)),
